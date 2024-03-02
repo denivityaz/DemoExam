6bb58ca2d2964f2d32a3b443668b5a03ac5e5b66
Denis <115842258+denivityaz@users.noreply.github.com>
2024-03-02 14:41:31 +0300

    Update 1_2.sql

--- 1_2.sql
+++ 1_2.sql
@@ -1,37 +1,22 @@
--- Создаем базу данных BD
-CREATE DATABASE BD;
+#!/bin/bash
 
--- Подключаемся к базе данных BD
-\c BD
+# Создаем базу данных BD
+psql -U postgres -d postgres -c "CREATE DATABASE BD;"
 
--- Создаем таблицу users для хранения логинов и паролей пользователей
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(50),
-    password VARCHAR(50)
-);
+# Подключаемся к базе данных BD
+psql -U postgres -d BD -c "\c BD;"
 
--- Создаем 10 баз данных BD1, BD2, ..., BD10
-DO $$
-DECLARE
-    i INT := 1;
-BEGIN
-    WHILE i <= 10 LOOP
-        EXECUTE format('CREATE DATABASE BD%s;', i);
-        i := i + 1;
-    END LOOP;
-END $$;
+# Создаем 10 баз данных BD1, BD2, ..., BD10
+psql -U postgres -d BD -c "DO $$ BEGIN FOR i IN 1..10 LOOP EXECUTE format('CREATE DATABASE BD%s;', i); END LOOP; END $$;"
 
+# Подключаемся к базе данных BD
+psql -U postgres -d BD -c "\c BD;"
 
--- Создаем 10 пользователей user1, user2, ..., user10 с случайными паролями и предоставляем им доступ только к их соответствующим базам данных
-DO $$
-DECLARE
-    random_password TEXT;
-BEGIN
-    FOR i IN 1..10 LOOP
-        random_password := substr(md5(random()::text), 0, 5);
-        
-        EXECUTE format('CREATE USER user%s WITH PASSWORD %L;', i, random_password);
-        EXECUTE format('GRANT ALL PRIVILEGES ON DATABASE BD%s TO user%s;', i, i);
-    END LOOP;
-END $$;
+# Создаем таблицу users для хранения логинов и паролей пользователей
+psql -U postgres -d BD -c "CREATE TABLE users (id SERIAL PRIMARY KEY, username VARCHAR(50), password VARCHAR(50));"
+
+# Создаем 10 пользователей user1, user2, ..., user10 с случайными паролями
+psql -U postgres -d BD -c "DO $$ DECLARE random_password TEXT; BEGIN FOR i IN 1..10 LOOP random_password := substring(md5(random()::text), 1, 5); EXECUTE format('CREATE USER user%s WITH PASSWORD %L;', i, random_password); END LOOP; END $$;"
+
+# Добавляем записи в таблицу users
+psql -U postgres -d BD -c "DO $$ BEGIN FOR i IN 1..10 LOOP EXECUTE format('INSERT INTO users (username, password) VALUES (%L, %L);', 'user' || i, substring(md5(random()::text), 1, 5)); END LOOP; END $$;"
